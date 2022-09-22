e037d5c37c84638f9f862e329ed9bc87157a1efb
NxtPerfect <alakaxan@protonmail.com>
2022-09-22 18:23:25 +0200

    Finished hangman game, used youtube example but changed it so that the input is read from the user

--- src/main.rs
+++ src/main.rs
@@ -6,83 +6,140 @@ use std::io;
 // ! the word has end line before the end of quotes
 // ! the word to guess isn't underscored for all the letters in it, instead it shows one letter that isn't in the guessed word
 
-pub fn guessing(hidden_word: &str, guess: char, original_word: &str, tries: usize) -> bool {
-    // we need to check which char was guessed and then reveal it
-    let mut is_correct = false;
-    println!(
-        "Your word to guess is \"{}\", with {} tries left",
-        guess, tries
-    );
-    for n in 1..hidden_word.len() {
-        if original_word.chars().nth(n).unwrap().eq(&guess) {
-            is_correct = true;
-        }
-    }
-    is_correct
+const ALLOWED_ATTEMPTS: u8 = 5;
+
+struct Letter {
+    character: char,
+    revealed: bool
+}
+
+enum GameProgress {
+    InProgress,
+    Won,
+    Lost
 }
 
 fn main() {
+    let mut turns_left = ALLOWED_ATTEMPTS;
+    let selected_word = select_word();
+    let mut letters = create_letters(&selected_word);
+    
     println!("Hello, welcome to hangman v1.0, please provide your word to guess");
-
-    let mut original_word = String::new();
-    // match is used to check if everything went good
+    
     loop {
-        match io::stdin().read_line(&mut original_word) {
-            // call iostream, read line to word by reference
-            Ok(_) => {
-                println!("Is this word correct? \"{}\"", original_word.to_lowercase());
-                break;
-            }
-            Err(e) => println!("Opps! something went wrong: {}", e),
-        }
-    }
+        println!("You have {} turns left", turns_left);
+        display_progress(&letters);
 
-    let mut hidden_word = String::new();
+        println!("Enter your letter");
+        let user_char = read_user_input_character();
 
-    for i in 1..original_word.len() {
-        hidden_word += "_";
-        // this needs to replace each char
-    }
-
-    let mut input = String::new();
-    let mut tries = hidden_word.chars().count();
-
-    while hidden_word.eq(&original_word.to_lowercase()) || tries > 0 {
-        loop {
-            match io::stdin().read_line(&mut input) {
-                Ok(_) => break,
-                Err(e) => println!(
-                    "Opps! Something went wrong, we did a little fucky wucky uwu {}",
-                    e
-                ),
-            }
-        }
-
-        if input.to_lowercase().eq(&original_word.to_lowercase()) {
-            println!("You guessed the word, good job!");
+        if user_char == '*' {
             break;
         }
 
-        let guess = input.chars().nth(0).unwrap();
-        if guessing(&hidden_word, guess, &original_word, tries) {
-            println!("That is correct!");
-            for i in 1..original_word.len() {
-                if original_word.chars().nth(i) == input.chars().nth(0) {
-                    hidden_word
-                        .chars()
-                        .nth(i)
-                        .clone_from(&original_word.chars().nth(i));
-                    break;
-                };
+        let mut at_least_one_revealed = false;
+        for letter in letters.iter_mut() {
+            if letter.character == user_char {
+                letter.revealed = true;
+                at_least_one_revealed = true;
             }
-            // ! reveal that letter in hidden_word
-            // ! search through guess for the letter we guessed
-            // ! if it's there then put that letter in hidden_word
-
-            continue;
         }
 
-        tries -= 1;
-        println!("Sadly that's a bad guess isn't in the word");
+        if !at_least_one_revealed {
+            turns_left -= 1;
+        }
+
+        match check_progress(turns_left, &letters) {
+            GameProgress::InProgress => continue,
+            GameProgress::Won => {
+                println!("Congrats! You guessed the word.");
+                break;
+            }
+            GameProgress::Lost => {
+                println!("You failed!");
+                break;
+            }
+        }
     }
+
+    println!("See you next time!");
+}
+
+fn select_word() -> String{
+    let mut user_input = String::new();
+    println!("Write the word that you want to guess");
+    match io::stdin().read_line(&mut user_input) {
+        Ok(_) => {
+           return user_input;
+        }
+        Err(_) => {
+           println!("Wrong input");
+           return String::from("Error");
+       }
+    }
+}
+
+fn create_letters(word: &String) -> Vec<Letter> {
+    let mut letters: Vec<Letter> = Vec::new();
+
+    for c in word.chars() {
+        letters.push(Letter {
+            character: c,
+            revealed: false
+        });
+    }
+
+    return letters;
+}
+
+fn display_progress(letters: &Vec<Letter>) {
+    let mut display_string = String::from("Progress:");
+
+    for letter in letters {
+        display_string.push(' ');
+
+        if letter.revealed {
+            display_string.push(letter.character);
+        }
+        else {
+            display_string.push('_');
+        }
+
+        display_string.push(' ');
+    }
+
+    println!("{}", display_string);
+}
+
+fn read_user_input_character() -> char {
+    let mut user_input = String::new();
+
+    match io::stdin().read_line(&mut user_input) {
+        Ok(_) => {
+            match user_input.chars().next() {
+                Some(c) => { return c; }
+                None => { return '*'; }
+            }
+        }
+        Err(_) => { return '*'; }
+    }
+}
+
+fn check_progress(turns_left: u8, letters: &Vec<Letter>) -> GameProgress {
+    let mut all_revealed = false;
+    for letter in letters {
+        if !letter.revealed {
+            all_revealed = false
+        }
+    }
+
+    if all_revealed {
+        return GameProgress::Won;
+    }
+
+    if turns_left > 0 {
+        return GameProgress::InProgress;
+    }
+
+    return GameProgress::Lost;
 }
